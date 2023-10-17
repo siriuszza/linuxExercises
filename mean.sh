4a99e7e9c79a960bda3d2fd499461bbacb0cf1bf
Xiaoyang Wang <xwang2684@wisc.edu>
2023-10-17 14:50:46 -0500

    update wxy's version

--- mean.sh
+++ mean.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+if [[ ($# -lt 1) || ($# -gt 2) ]]; then
+	echo "usage: ./mean.sh <column> [file.csv]" 1>&2
+	exit 0
+fi
+
+column=$1
+
+if [[ $# -eq 1 ]]; then
+	file=/dev/stdin
+else
+	file=$2
+fi
+
+data=$(tail -n +2 $file | cut -d ',' -f $column)
+
+sum=0
+line_count=0
+
+while read sample; do
+	((sum+=sample))
+	((line_count+=1))
+done <<< "$data"
+
+mean=$(bc <<< "scale=4; $sum / $line_count")
+echo $mean
