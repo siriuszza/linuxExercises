ffd1cc9449ddf2f6b0fd3763cd62ea3650390ee2
Yanrun Lu <ylu438@wisc.edu>
2023-10-17 13:45:13 -0500

    I finished school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+total_values=$(cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d',' -f7)
+sum=$(echo "$total_values" | awk '{ sum += $1 } END { print sum }')
+count=$(echo "$total_values" | wc -l)
+average=$(echo "scale=2; $sum / $count" | bc)
+echo "Average TotalAssessedValue: $average"
